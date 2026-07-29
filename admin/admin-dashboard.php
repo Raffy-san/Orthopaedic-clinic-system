50ebff1e4a7ffdc404dc06a390852f42412f8bef
Rafael Sanoria <rafaelsanoria506@gmail.com>
2026-07-29 21:56:03 +0800

    On branch raffy  Your branch is up to date with 'origin/raffy'. Changes to be committed: 	new file:   admin/admin-dashboard.php 	renamed:    css/global.css -> assets/css/global.css 	renamed:    css/input.css -> assets/css/input.css 	renamed:    css/output.css -> assets/css/output.css 	new file:   includes/sidebar.php 	renamed:    index.html -> index.php 	modified:   package.json 	deleted:    sidebar.html

---
+++ admin/admin-dashboard.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../assets/css/output.css">
+    <link rel="stylesheet" href="../assets/css/global.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <title>Document</title>
+</head>
+
+<body class="bg-gray-200 h-screen flex">
+    <?php include_once '../includes/sidebar.php'; ?>
+    <section class="p-6">
+        <div>
+            <h1 class="text-3xl font-bold">Dashboard</h1>
+            <h3 class="text-lg font-medium text-gray-500">Wednesday, July 29, 2026 - Welcome, Admin</h3>
+        </div>
+        <div>
+
+        </div>
+    </section>
+</body>
+
+</html>
